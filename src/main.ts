bbbed336b0a0ddb9e4463e3fb1ec6a17b07cf78f
johe lappy <johevin.blesstowi@binus.ac.id>
2024-06-26 21:18:49 +0700

    fix main ts configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
+  // Set up Swagger
   const config = new DocumentBuilder()
     .setTitle('ECOS Swagger Backend')
     .setVersion('1.0')
@@ -14,7 +14,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Enable CORS
   app.enableCors();
+
+  // Enable validation globally
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
