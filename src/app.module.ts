a1a463601e9465116e1d6f4f17f29371429e2495
johe lappy <johevin.blesstowi@binus.ac.id>
2024-06-30 23:11:09 +0700

    add public images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserDetailModule } from './api/user_detail/user_detail.module';
 import { AuthModule } from './api/auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UserDetailModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    AuthModule,
+    UserDetailModule,
+  ],
 })
 export class AppModule {}
